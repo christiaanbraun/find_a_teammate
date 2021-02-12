0e2735b862f322f53545ad1f808bb8b5bcc2f5f1
christiaanbraun <christiaan.braun@hva.nl>
2021-02-12 12:13:45 +0100

    removed test dir

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-
-http.createServer(onrequest).listen(8000)
-
-function onrequest(req, res) {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text.html')
-    res.end('Hello World!\n')
-}
