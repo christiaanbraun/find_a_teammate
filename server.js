450c4651a63cf075cf6566c4a1f2c42117d80195
christiaanbraun <christiaan.braun@hva.nl>
2021-02-18 09:18:03 +0100

    Add partials and fix routing

--- server.js
+++ server.js
@@ -4,32 +4,36 @@ const port = 5000
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs')
-// Set views as views dir
-
-// Get homepage
-app.get('/', (req, res) => {
-   res.render('home')
-})
-
-// Set about page
-app.get('/profile', (req, res) => {
-    res.render('my_profile')
- })
-
-// Set login page
-app.get('/settings', (req, res) => {
-    res.render('settings')
- })
-
-// Set 404 page
-app.use(function(req, res){
-    res.status(404).send('404 not found')
-})
 
 // Static files are in public folder
 app.use(express.static('public'))
 
+// Get homepage
+app.get('/', (req, res) => {
+   res.render('pages/home', {
+    username: 'ChrisBreezy',
+    age: '21',
+    favoritePrimary: 'AN-94',
+    favoriteSecondary: 'Combat Knife'
+})
+})
+
+// Get about page
+app.get('/profile', (req, res) => {
+    res.render('pages/my_profile')
+ })
+
+// Get login page
+app.get('/settings', (req, res) => {
+    res.render('pages/settings')
+ })
+
+// Get 404 page
+app.use(function(req, res){
+    res.status(404).send('404 not found')
+})
+
 // Set port and verify the server is working
 app.listen(port, function(){
-    console.log(`Server is up and running on ${port}`)
+    console.log(`Server is up and running on localhost:${port}`)
 })
