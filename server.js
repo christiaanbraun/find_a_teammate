0c457b47c18ae873c5be0cd3a6278a9e8969a7db
christiaanbraun <christiaan.braun@hva.nl>
2021-03-03 13:06:20 +0100

    Clear cache to remove node modules

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-const dotenv = require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+
 const controller = require('./controller/controller.js');
 
 const app = express();
 const port = 5000;
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@buster.boqc9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-const { connection } = mongoose;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
